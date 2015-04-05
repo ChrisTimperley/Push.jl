d33365ae2bd35468ec491a14729c3eae21f20f74
ChrisTimperley <christimperley@googlemail.com>
2015-04-05 20:58:09 +0100

    Sketched outline of parser.

---
+++ parser.jl
@@ -0,0 +1,13 @@
+module Parser
+  export to_push
+
+  # Produces a readily executable Push program from a given definition.
+  to_push(s::String) = return
+
+  # Lex source code into a sequence of tokens.
+  lex(p::String) = return
+
+  # Parse tokens into a Push program.
+  parse(p::String) = return
+
+end
