73d644f33116b7239ec57046d5f5c31d9a324253
ChrisTimperley <christimperley@googlemail.com>
2015-04-06 21:00:17 +0100

    Added instruction table and constructors to State.

--- src/state.jl
+++ src/state.jl
@@ -4,4 +4,10 @@ type State
   boolean::Vector{Bool}
   code::Vector{Any}
   exec::Vector{Any}
+  instructions::Dict{Symbol, Function}
+  
+  State() = new()
+  State(f::Vector{Float32}, i::Vector{Int32}, b::Vector{Int32},
+    c::Vector{Any}, e::Vector{Any}, ins::Dict{Symbol, Function}) =
+    new(f, i, b, c, e, ins)
 end
