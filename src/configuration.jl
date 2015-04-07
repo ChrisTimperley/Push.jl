d9d7eaf63a389a3641b4f645b4c4606914751ebb
ChrisTimperley <christimperley@googlemail.com>
2015-04-07 16:17:28 +0100

    Implemented configurations and configuration loading.

--- src/configuration.jl
+++ src/configuration.jl
@@ -1,21 +1,48 @@
 type Configuration
+  parameters::Dict{String, String}
   types::Vector{String}
   instructions::Vector{Symbol}
+
+  Configuration() = new(Dict{String, Any}(), String[], Symbol[])
+end
+
+# Loads a configuration file at a given location.
+load_configuration(f::String) = open(load_configuration, f)
+function load_configuration(f::File)
+  c = Configuration()
+  for ln in eachline(f)
+    ln = strip(ln)
+
+    # Ignore comments and empty lines.
+    if !isempty(ln) || ln[1] == '#'
+      k, v = split(s)
+      if k == "type" # Type declaration.
+        continue
+      elseif k == "instruction" # Instruction declaration.
+        push!(c.instructions, convert(Symbol, v))
+      else # Parameter definition.
+        c.parameters[k] = v
+      end
+    endW
+  end
+  return c
 end
 
 # Creates and returns an initial state using a given configuration.
+configure(f::String) = configure(load_configuration(f))
 function configure(c::Configuration)
   s = State()
 
   # Instruction handling.
   for i in c.instructions
-    enable_instruction!(state, i)
+    enable_instruction!(s, i)
   end
 
   # Type handling.
   # - Means nothing for now.
 
   # Parameter handling.
+  set!(s.parameters, c.parameters)
 
   return s
 end
