cd7bb2a508622c431319385363019ed21ec2e098
ChrisTimperley <christimperley@googlemail.com>
2015-04-08 23:59:56 +0100

    Added stack pretty printer.

--- src/state.jl
+++ src/state.jl
@@ -15,6 +15,15 @@ type State
     new(f, i, b, c, e, ins)
 end
 
+function pp_stacks(s::State)
+  println("FLOAT: ($(join(s.float, " ")))")
+  println("INTEGER: ($(join(s.integer, " ")))")
+  println("BOOLEAN: ($(join(s.boolean, " ")))")
+  println("NAME: ($(join(s.name, " ")))")
+  println("CODE: ($(join(s.code, " ")))")
+  println("EXEC: ($(join(s.exec, " ")))")
+end
+
 # Enables a registered Push instruction, specified by its name,
 # by inserting it into the instruction table of a given state.
 enable_instruction!(s::State, i::String) = enable_instruction!(s, convert(Symbol, i))
