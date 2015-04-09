7729032e2f6e7544c75567e3f0b784b231e8b8c3
ChrisTimperley <christimperley@googlemail.com>
2015-04-09 21:24:16 +0100

    Wrote unit tests for name instructions.

---
+++ test/test_name.jl
@@ -0,0 +1,93 @@
+require("src/Push.jl")
+
+using Push
+using Base.Test
+
+cfg_path = joinpath(dirname(@__FILE__), "configuration/name.cfg")
+cfg = Push.load_configuration(cfg_path)
+
+# NAME.QUOTE
+# - Check against Push specification.
+s = Push.run("(NAME.QUOTE NAME.QUOTE)", cfg)
+@test s.name == [convert(Symbol, "NAME.QUOTE")]
+
+# NAME.=
+s = Push.run("(X Y NAME.=)", cfg)
+@test s.boolean == [false]
+s = Push.run("(X X NAME.=)", cfg)
+@test s.boolean == [true]
+
+# NAME.DUP
+s = Push.run("(X NAME.DUP)", cfg)
+@test s.name == [:X, :X]
+
+# NAME.FLUSH
+s = Push.run("(X Y Z NAME.FLUSH)", cfg)
+@test isempty(s.name)
+
+# NAME.POP
+s = Push.run("(X Y Z NAME.POP)", cfg)
+@test s.name == [:X, :Y]
+
+# NAME.ROT
+s = Push.run("(Y Z NAME.ROT)", cfg)
+@test s.name == [:Y, :Z]
+s = Push.run("(A X Y Z NAME.ROT)", cfg)
+@test s.name == [:A, :Z, :Y, :X]
+
+# NAME.SHOVE
+s = Push.run("(A B C D 0 NAME.SHOVE)", cfg)
+@test s.name == [:A, :B, :C, :D]
+s = Push.run("(A B C D -100 NAME.SHOVE)", cfg)
+@test s.name == [:A, :B, :C, :D]
+s = Push.run("(A B C D 1 NAME.SHOVE)", cfg)
+@test s.name == [:A, :B, :D, :C]
+s = Push.run("(A B C D 2 NAME.SHOVE)", cfg)
+@test s.name == [:A, :D, :B, :C]
+s = Push.run("(A B C D 3 NAME.SHOVE)", cfg)
+@test s.name == [:D, :A, :B, :C]
+s = Push.run("(A B C D 10 NAME.SHOVE)", cfg)
+@test s.name == [:D, :A, :B, :C]
+
+# NAME.STACKDEPTH
+s = Push.run("(A B C D NAME.STACKDEPTH)", cfg)
+@test s.name == [:A, :B, :C, :D] && s.integer == [4]
+s = Push.run("(NAME.STACKDEPTH)", cfg)
+@test s.integer == [0]
+
+# NAME.SWAP
+s = Push.run("(A B C NAME.SWAP)", cfg)
+@test s.name == [:A, :C, :B]
+s = Push.run("(A NAME.SWAP)", cfg)
+@test s.name == [:A]
+
+# NAME.YANK
+s = Push.run("(A B C D 0 NAME.YANK)", cfg)
+@test s.name == [:A, :B, :C, :D]
+s = Push.run("(A B C D -100 NAME.YANK)", cfg)
+@test s.name == [:A, :B, :C, :D]
+s = Push.run("(A B C D 1 NAME.YANK)", cfg)
+@test s.name == [:A, :B, :D, :C]
+s = Push.run("(A B C D 2 NAME.YANK)", cfg)
+@test s.name == [:A, :C, :D, :B]
+s = Push.run("(A B C D 3 NAME.YANK)", cfg)
+@test s.name == [:B, :C, :D, :A]
+s = Push.run("(A B C D 10 NAME.YANK)", cfg)
+@test s.name == [:B, :C, :D, :A]
+
+# NAME.YANKDUP
+s = Push.run("(A B C D 0 NAME.YANKDUP)", cfg)
+@test s.name == [:A, :B, :C, :D, :D]
+s = Push.run("(A B C D -100 NAME.YANKDUP)", cfg)
+@test s.name == [:A, :B, :C, :D, :D]
+s = Push.run("(A B C D 1 NAME.YANKDUP)", cfg)
+@test s.name == [:A, :B, :C, :D, :C]
+s = Push.run("(A B C D 2 NAME.YANKDUP)", cfg)
+@test s.name == [:A, :B, :C, :D, :B]
+s = Push.run("(A B C D 3 NAME.YANKDUP)", cfg)
+@test s.name == [:A, :B, :C, :D, :A]
+s = Push.run("(A B C D 10 NAME.YANKDUP)", cfg)
+@test s.name == [:A, :B, :C, :D, :A]
+
+# NAME.RAND
+# NAME.RANDBOUNDNAME
