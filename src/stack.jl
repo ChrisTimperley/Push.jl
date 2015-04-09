3e601f8c98a79aadf0bc8e55c417f0f7492a870a
ChrisTimperley <christimperley@googlemail.com>
2015-04-09 14:58:35 +0100

    Implemented common stack operations: yank, yankdup and shove.

--- src/stack.jl
+++ src/stack.jl
@@ -1,1 +1,20 @@
 peek{T}(v::Vector{T}) = v[end]
+
+function shove!{T}(a::Vector{T}, i::Integer)
+  ln = length(a)
+  i = min(max(1, ln - i), ln + 1)
+  pop!(insert!(a, i, a[end]))
+  return a
+end
+
+function yank!{T}(a::Vector{T}, i::Integer)
+  ln = length(a)
+  i = min(max(1, ln - i), ln)
+  push!(a, splice!(a, i))
+end
+
+function yankdup!{T}(a::Vector{T}, i::Integer)
+  ln = length(a)
+  i = min(max(1, ln - i), ln)
+  push!(a, a[i])
+end
