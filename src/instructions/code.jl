f2ee78088ecebddf3ec4fb136a3399887427a794
ChrisTimperley <christimperley@googlemail.com>
2015-05-18 02:24:02 +0100

    Fixed CODE.ROT instruction.

--- src/instructions/code.jl
+++ src/instructions/code.jl
@@ -194,7 +194,7 @@ CODE_QUOTE(s::State) = s.flag_quote_code = true
 
 CODE_ROT(s::State) = if length(s.code) > 2
   s.code[end], s.code[end-1], s.code[end-2] =
-    s.code[end-2], s.code[end], s.code[end-1]
+    s.code[end-1], s.code[end-2], s.code[end]
 end
 
 #
